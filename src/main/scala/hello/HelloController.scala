58661752c3d2f374ea4b8592241d074519f5b6ec
Karthik Sudhakar <kkcarthieckk@gmail.com>
2015-02-11 12:16:55 -0800

    Importing helloworld

---
+++ src/main/scala/hello/HelloController.scala
@@ -0,0 +1,24 @@
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.web.bind.annotation.{RequestMapping, RestController}
+import org.springframework.boot._;
+import org.springframework.boot.autoconfigure._;
+import org.springframework.web.bind.annotation._;
+@RestController
+@EnableAutoConfiguration
+@ComponentScan
+class HelloController {
+
+   @RequestMapping(Array("/"))
+   def newresponse() = "hello-world...hello-scala!!"
+}
+
+object Controller {
+
+    def main(args: Array[String]) {
+    SpringApplication.run(classOf[HelloController]);
+        }
+}
+ 
+1
